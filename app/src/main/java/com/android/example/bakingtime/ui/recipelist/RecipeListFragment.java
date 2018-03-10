57a10c6710a1e78603bb920491034330247b54f6
Ravi Tiwari <rvtwr123@gmail.com>
2018-03-10 22:37:28 +0530

    Fixed this bug, if the device is not connected to internet and the list is null the app crashes on rotation.

--- app/src/main/java/com/android/example/bakingtime/ui/recipelist/RecipeListFragment.java
+++ app/src/main/java/com/android/example/bakingtime/ui/recipelist/RecipeListFragment.java
@@ -2,10 +2,13 @@ package com.android.example.bakingtime.ui.recipelist;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,7 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.android.example.bakingtime.R;
 import com.android.example.bakingtime.data.model.Recipe;
@@ -39,6 +42,7 @@ public class RecipeListFragment extends Fragment implements RecipeDownloadTask.T
     private ArrayList<Recipe> recipes;
     private Parcelable recipeListState;
     private ProgressBar loadingIndicator;
+    private TextView noRecipeTextView;
     private RecyclerView recipeRecyclerView;
     private RecipeAdapter recipeAdapter;
 
@@ -64,8 +68,18 @@ public class RecipeListFragment extends Fragment implements RecipeDownloadTask.T
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
 
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(STATE_RECIPE_SCROLL))
+                recipeListState = savedInstanceState.getParcelable(STATE_RECIPE_SCROLL);
+            if (savedInstanceState.containsKey(STATE_RECIPE_LIST))
+                recipes = savedInstanceState.getParcelableArrayList(STATE_RECIPE_LIST);
+        }
+
         int spanCount = getResources().getInteger(R.integer.recipes_column_count);
         GridLayoutManager layoutManager = new GridLayoutManager(context, spanCount);
+        if (recipeListState != null) layoutManager.onRestoreInstanceState(recipeListState);
+
+        noRecipeTextView = view.findViewById(R.id.tv_no_recipe);
 
         recipeRecyclerView = view.findViewById(R.id.rv_recipes);
         recipeRecyclerView.setLayoutManager(layoutManager);
@@ -73,18 +87,8 @@ public class RecipeListFragment extends Fragment implements RecipeDownloadTask.T
 
         loadingIndicator = view.findViewById(R.id.loading_indicator);
 
-        if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey(STATE_RECIPE_SCROLL)) {
-                recipeListState = savedInstanceState.getParcelable(STATE_RECIPE_SCROLL);
-                recipeRecyclerView.getLayoutManager().onRestoreInstanceState(recipeListState);
-            }
-            if (savedInstanceState.containsKey(STATE_RECIPE_LIST)) {
-                recipes = savedInstanceState.getParcelableArrayList(STATE_RECIPE_LIST);
-                setupRecipeAdapter();
-            }
-        } else {
-            new RecipeDownloadTask(this).execute(RECIPE_URL);
-        }
+        if (recipes == null || recipes.isEmpty()) downloadRecipes();
+        else setupRecipeAdapter();
 
         return view;
     }
@@ -113,8 +117,27 @@ public class RecipeListFragment extends Fragment implements RecipeDownloadTask.T
         }
     }
 
+    private void downloadRecipes() {
+        if (isInternetAvailable()) {
+            showRecipeView();
+            new RecipeDownloadTask(this).execute(RECIPE_URL);
+        } else {
+            showNoRecipeView();
+            Snackbar snackbar = Snackbar.make(recipeRecyclerView, R.string.no_internet_message,
+                    Snackbar.LENGTH_INDEFINITE);
+            snackbar.setAction("Retry", v -> downloadRecipes());
+            snackbar.show();
+        }
+    }
+
     private void showNoRecipeView() {
-        Toast.makeText(context, "No recipe found :(", Toast.LENGTH_SHORT).show();
+        recipeRecyclerView.setVisibility(View.GONE);
+        noRecipeTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showRecipeView() {
+        noRecipeTextView.setVisibility(View.GONE);
+        recipeRecyclerView.setVisibility(View.VISIBLE);
     }
 
     private void setupRecipeAdapter() {
@@ -129,5 +152,16 @@ public class RecipeListFragment extends Fragment implements RecipeDownloadTask.T
     private void onRecipeSelected(@NonNull final Recipe recipe) {
         Intent intent = RecipeActivity.newIntent(context, recipe);
         startActivity(intent);
+    }
+
+    private boolean isInternetAvailable() {
+        boolean isAvailable = false;
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            isAvailable = networkInfo != null && networkInfo.isConnected();
+        }
+        return isAvailable;
     }
 }
